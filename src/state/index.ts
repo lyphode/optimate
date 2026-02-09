da42a6ef945d0fecb44071823910898775379bce
Rik <richard.bridgstock@gmail.com>
2026-02-09 15:14:33 +0200

    Initial commit

---
+++ src/state/index.ts
@@ -0,0 +1,9 @@
+// State layer barrel file.
+// Exposes auth, slab, part, and nesting hooks/services to the UI.
+// IMPORTANT: Do not instantiate classes or singletons here.
+
+export * from "./auth";
+export * from "./slabs";
+export * from "./parts";
+export * from "./nesting";
+
